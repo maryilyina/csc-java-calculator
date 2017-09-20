d2ec159e71214d2d65c8268265ebfba848a38122
frikadelki <alexander.kazanin@gmail.com>
2017-09-21 04:40:40 +0700

    core#1: Basic main.

---
+++ src/main/java/org/csc/nsk/java2017/task00/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Task 00
+ * CSC Java 2017
+ * Created by frikadelki on 2017/9/21
+ */
+
+package org.csc.nsk.java2017.task00;
+
+public class Main {
+	public static void main(String[] args) {
+		final Main main = new Main();
+		main.run();
+	}
+
+	private Main() {
+	}
+
+	private void run() {
+		System.out.println("Hello world.");
+	}
+}
