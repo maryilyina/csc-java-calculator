2b859426758da6b097355a9ead957503175fdb21
frikadelki <frikadelki@noop>
2017-10-05 02:50:43 +0700

    task01: removed lombok from unit-tests.

--- task-01-containers/src/teacher-test/java/org/csc/nsk/java2017/task01/MultisetTeacherTestsBasic.java
+++ task-01-containers/src/teacher-test/java/org/csc/nsk/java2017/task01/MultisetTeacherTestsBasic.java
@@ -6,23 +6,24 @@
 
 package org.csc.nsk.java2017.task01;
 
-import lombok.val;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Iterator;
 
 public final class MultisetTeacherTestsBasic {
     @Test
     public void testSingleAdd() {
-        val testValue = new Object();
+        final Object testValue = new Object();
 
-        val multiset = MultisetFactory.makeDefaultMultiset();
+        final Multiset<Object> multiset = MultisetFactory.makeDefaultMultiset();
         multiset.add(testValue);
 
         Assert.assertTrue("Bad multiset size.", multiset.size() == 1);
         Assert.assertTrue("Multiset is empty.", !multiset.isEmpty());
         Assert.assertTrue("Multiset did not contain added value.", multiset.contains(testValue));
 
-        val iterator = multiset.iterator();
+        final Iterator<Object> iterator = multiset.iterator();
         Assert.assertTrue("Iterator is empty.", iterator.hasNext());
         Assert.assertSame("Iterator did not return expected element.", testValue, iterator.next());
         Assert.assertFalse("Iterator reports more values than was added.", iterator.hasNext());
